1b1583889568c0bc093d8cf94b628030a3d23edc
shubham <shubham.shreekumar@gmail.com>
2025-09-05 23:15:55 +0530

    Closing CTA section

---
+++ Client/components/ClosingCTA/ClosingCTA.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ClosingCTA.css";
+
+const ClosingCTA = () => (
+  <section className="closing-cta-section">
+    <div className="closing-cta-content">
+      <h2 className="closing-cta-text">
+        Discover your city’s soundscape today.
+      </h2>
+      <div className="closing-cta-buttons">
+        <button className="hero-btn primary">View City Noise Stats</button>
+        <button className="hero-btn secondary">Analyze My Audio</button>
+      </div>
+    </div>
+  </section>
+);
+
+export default ClosingCTA;
