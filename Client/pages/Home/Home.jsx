94230373478a35c8a0e6b0c890c9d4a3a2302848
shubham <shubham.shreekumar@gmail.com>
2025-09-05 23:17:24 +0530

    Added components to Home.jsx

--- Client/pages/Home/Home.jsx
+++ Client/pages/Home/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Hero from "../../components/Hero/Hero.jsx";
+import About from "../../components/About/About.jsx";
+import Demo from "../../components/Demo/Demo.jsx";
+import Working from "../../components/Working/Working.jsx";
+import ClosingCTA from "../../components/ClosingCTA/ClosingCTA.jsx";
+import Footer from "../../components/Footer/Footer.jsx";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Demo />
+      <Working />
+      <ClosingCTA />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
